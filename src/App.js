45eb152a560f942fb433d22e41457c3090fa828f
khanarezamaulana <khanarezamaulana@gmail.com>
2019-10-26 15:58:10 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactToExcel from 'react-html-table-to-excel'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <table border='1' id='table-to-xls'>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Khana</td>
+              <td>Reza</td>
+              <td>22</td>
+            </tr>
+            <tr>
+              <td>Khana</td>
+              <td>Reza</td>
+              <td>22</td>
+            </tr>
+            <tr>
+              <td>Khana</td>
+              <td>Reza</td>
+              <td>22</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <ReactToExcel 
+          className='download-table-xls-button'
+          table='table-to-xls'
+          filename='excelFile'
+          sheet='sheet 1'
+          buttonText='Download as XLS'
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
